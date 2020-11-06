373177f8a18cd2ef9ae424a0e0118f562dc94621
ShahzebINC <73095630+ShahzebINC@users.noreply.github.com>
2020-11-05 21:54:51 -0800

    Update App.js

--- devopsassignment3/src/App.js
+++ devopsassignment3/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kindpng.com%2Fimgv%2FTRhJJho_cats-logo-png-black-cat-mascot-logo-transparent%2F&psig=AOvVaw17g8UP5VezWbuuU-Th9jpW&ust=1604728384771000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJix1Jyd7ewCFQAAAAAdAAAAABAD"} className="App-logo" alt="logo" />
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kindpng.com%2Fimgv%2FTRhJJho_cats-logo-png-black-cat-mascot-logo-transparent%2F&psig=AOvVaw17g8UP5VezWbuuU-Th9jpW&ust=1604728384771000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJix1Jyd7ewCFQAAAAAdAAAAABAD" className="App-logo" alt="logo" />
         <p>
           Shahzeb Assignment #3
           <br></br>
